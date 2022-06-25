a1946368e9a06470ff74f7c886f2dfb79c9f2089
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-06-25 18:43:30 -0300

    controller e model de usuario e propriedade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user', function () {
+    return (new UserController)->index();
+});
+
+Route::get('/property', function () {
+    return (new PropertyController)->index();
 });
