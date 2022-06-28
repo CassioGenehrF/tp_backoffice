ff59c65bb6559a6384bf0a16227913232b6acd95
Micheli Ferreira <micheliferreira846@gmail.com>
2022-06-28 16:18:58 -0300

    Criacao tela de login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 })->name('login');
 
 Route::middleware(['auth', 'auth.session'])->group(function () {
