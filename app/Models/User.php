034dd97cf5edd020003d6563a16445b311b9f4be
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-07-02 12:30:26 -0300

    ajuste model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'wp_users';
+    
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'ID',
+        'user_login',
+        'user_pass',
+        'user_nicename',
+        'user_email',
+        'user_url',
+        'user_registered',
+        'user_activation_key',
+        'user_status',
+        'display_name'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $appends = ['role'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected function getRoleAttribute(): string
+    {
+        $role = DB::query()
+            ->select('meta_value')
+            ->from('wp_usermeta')
+            ->where('meta_key', 'wp_capabilities')
+            ->where('user_id', $this->ID)
+            ->first();
+        
+        $role = $role ? substr(substr($role->meta_value, 0, -7), 11) : '';
+        
+        return $role;
+    }
+
+    public function properties(): HasMany
+    {
+        return $this->hasMany(Property::class, 'post_author', 'ID');
+    }
 }
