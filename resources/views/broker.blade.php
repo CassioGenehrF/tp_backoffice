fd777c32757c225d7cde71f0e30a7c07dfa82a77
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-07-11 11:52:46 -0300

    ajuste campo adultos e criancas

--- resources/views/broker.blade.php
+++ resources/views/broker.blade.php
@@ -131,54 +131,11 @@
                 </div>
                 <div class="form-group col-md-4">
                     <label for="adultos">Adultos:</label>
-                    <select name="adultos" id="adultos" class="form-control" required>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                    </select>
+                    <input class="form-control" type="number" id="adultos" name="adultos" required>
                 </div>
                 <div class="form-group col-md-4">
                     <label for="criancas">Crianças:</label>
-                    <select name="criancas" id="criancas" class="form-control" required>
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                    </select>
+                    <input class="form-control" type="number" id="criancas" name="criancas" required>
                 </div>
             </div>
             <div class="custom-file mt-2">
