1ac5905a476b9d3de5124b779b2070e859c845d9
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-07-17 16:25:09 -0300

    ajuste report proprietario

--- resources/views/owner-report.blade.php
+++ resources/views/owner-report.blade.php
@@ -80,7 +80,7 @@
                     <th scope="col">Diárias</th>
                     <th scope="col">Total</th>
                     <th scope="col">Taxa de Anfitrião</th>
-                    <th id="comission" scope="col">Comissão</th>
+                    <th id="comission" scope="col">Ganhos sob Indicação</th>
                 </tr>
             </thead>
             <tbody id="report-content">
