1f311f71786dfe931eef67482ce3bce67de4f1e9
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-08-11 21:27:00 -0300

    ajuste filtros

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -11,18 +11,35 @@
                     <input type="date" class="form-control" name="end" id="end" wire:model="end">
                 </div>
             </div>
-            @foreach ($filters as $title => $filter)
-                <div class="row col-md-12 mt-2">
-                    <h2 class="col-md-12">{{ $title }}</h2>
-                    @foreach ($filter as $input)
-                        <div class="form-check col-md-3">
-                            <input type="checkbox" class="form-check-input" id="{{ $input['slug'] }}"
-                                name="{{ $input['slug'] }}" value="{{ $input['term_id'] }}" wire:model="terms">
-                            <label for="{{ $input['slug'] }}" class="form-check-label">{{ $input['name'] }}</label>
+            <div class="accordion mt-2" id="filters">
+                @foreach ($filters as $title => $filter)
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="heading{{ Str::slug($title) }}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#{{ Str::slug($title) }}" aria-expanded="false"
+                                aria-controls="{{ Str::slug($title) }}">
+                                {{ $title }}
+                            </button>
+                        </h2>
+                        <div id="{{ Str::slug($title) }}" class="accordion-collapse collapse"
+                            aria-labelledby="heading{{ Str::slug($title) }}" data-bs-parent="#filters">
+                            <div class="accordion-body">
+                                <div class="row col-md-12">
+                                    @foreach ($filter as $input)
+                                        <div class="form-check col-md-3">
+                                            <input type="checkbox" class="form-check-input" id="{{ $input['slug'] }}"
+                                                name="{{ $input['slug'] }}" value="{{ $input['term_id'] }}"
+                                                wire:model="terms">
+                                            <label for="{{ $input['slug'] }}"
+                                                class="form-check-label">{{ $input['name'] }}</label>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
                         </div>
-                    @endforeach
-                </div>
-            @endforeach
+                    </div>
+                @endforeach
+            </div>
         </div>
         <div class="table-responsive">
             <table class="table table-striped">
